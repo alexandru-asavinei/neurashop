c52706a4783c49a1b47677c845821ab566602ac8
Alexandru Asavinei <55081223+alexandru-asavinei@users.noreply.github.com>
2026-04-20 18:35:19 +0200

    feat(cart): add dynamic cart badge in mobile navigation

--- frontend/components/MobileNav.tsx
+++ frontend/components/MobileNav.tsx
@@ -1,39 +1,49 @@
 "use client";
 
 import Link from "next/link";
+import { useCart } from "../context/CartContext";
 
 export default function MobileNav() {
+  const { cart } = useCart();
   return (
     <div className="fixed bottom-0 left-0 right-0 max-w-md mx-auto bg-white border-t border-gray-200 flex justify-around py-3 z-50">
-      
+
       <Link href="/">
         <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
-  Home
-</button>
+          Home
+        </button>
       </Link>
 
       <Link href="/search">
         <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
-  Search
-</button>
+          Search
+        </button>
       </Link>
 
       <Link href="/cart">
-   <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
-  Cart
-</button>
+        <div className="relative cursor-pointer">
+          <button className="text-sm text-gray-600 hover:text-black transition active:scale-95">
+            Cart
+          </button>
+
+          {cart.length > 0 && (
+            <span className="absolute -top-2 -right-3 bg-black text-white text-xs px-2 py-0.5 rounded-full">
+              {cart.length}
+            </span>
+          )}
+        </div>
       </Link>
 
       <Link href="/wishlist">
-       <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
-  Wishlist
-</button>
+        <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
+          Wishlist
+        </button>
       </Link>
 
       <Link href="/profile">
-       <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
-  Profile
-</button>
+        <button className="text-sm text-gray-600 cursor-pointer hover:text-black transition active:scale-95">
+          Profile
+        </button>
       </Link>
 
     </div>
